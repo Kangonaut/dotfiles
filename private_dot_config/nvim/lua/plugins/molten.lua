480781b1e37b6d9602740d8ce3e9b6693c39070a
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-08-24 21:44:49 +0200

    nvim: configure molten for quarto

--- private_dot_config/nvim/lua/plugins/molten.lua
+++ private_dot_config/nvim/lua/plugins/molten.lua
@@ -2,23 +2,34 @@
 -- desc: a neovim plugin for interactively running code with the jupyter kernel (fork of magma.nvim)
 -- repo: https://github.com/benlubas/molten-nvim
 
-return {}
--- return {
---   "benlubas/molten-nvim",
---   version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
---   dependencies = { "3rd/image.nvim" },
---   build = ":UpdateRemotePlugins",
---   keys = {
---     { "<leader>me", ":MoltenEvaluateOperator<CR>",      { desc = "evaluate operator", silten = true } },
---     { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", { desc = "open output windows", silten = true } },
---   },
---   init = function()
---     -- these are examples, not defaults. Please see the readme
---     vim.g.molten_image_provider = "image.nvim"
---     vim.g.molten_output_win_max_height = 20
---
---     vim.g.molten_wrap_output = true
---     vim.g.molten_virt_text_output = true
---     vim.g.molten_virt_lines_off_by_1 = true
---   end,
--- }
+return {
+  "benlubas/molten-nvim",
+  version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+  dependencies = { "3rd/image.nvim" },
+  build = ":UpdateRemotePlugins",
+  ft = { "quarto", "markdown" },
+  init = function()
+    -- these are examples, not defaults. Please see the readme
+    vim.g.molten_image_provider = "image.nvim"
+    vim.g.molten_output_win_max_height = 20
+
+    vim.g.molten_wrap_output = true
+    vim.g.molten_virt_text_output = true
+    vim.g.molten_virt_lines_off_by_1 = true
+  end,
+  config = function()
+    require("which-key").add({
+      { "<leader>m", group = "[m]olten" },
+      { "<leader>m_", hidden = true },
+    })
+
+    vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { desc = "[m]olten [i]nit", silent = true })
+    vim.keymap.set("n", "<leader>me", ":MoltenEvaluateOperator<CR>", { desc = "[m]olten [e]valuate operator", silent = true })
+    vim.keymap.set("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { desc = "[m]olten evaluate [l]ine", silent = true })
+    vim.keymap.set("v", "<leader>me", ":<C-u>MoltenEvaluateVisual<CR>", { desc = "[m]olten evaluate [v]isual selection", silent = true })
+    vim.keymap.set("n", "<leader>mr", ":MoltenReevaluateCell<CR>", { desc = "[m]olten [r]e-evaluate cell", silent = true })
+    vim.keymap.set("n", "<leader>md", ":MoltenDelete<CR>", { desc = "[m]olten [d]elete", silent = true })
+    vim.keymap.set("n", "<leader>moh", ":MoltenHideOutput<CR>", { desc = "[m]olten [o]utput [h]ide", silent = true })
+    vim.keymap.set("n", "<leader>moe", ":MoltenHideOutput<CR>", { desc = "[m]olten [o]utput [e]nter", silent = true })
+  end,
+}
--- private_dot_config/nvim/lua/plugins/which-key.lua
+++ private_dot_config/nvim/lua/plugins/which-key.lua
@@ -13,29 +13,31 @@ return {
 
       -- document existing key chains
       require("which-key").add({
-        { "<leader>b",   group = "[b]uffer" },
-        { "<leader>b_",  hidden = true },
-        { "<leader>bc",  group = "[c]lose" },
+        { "<leader>p", group = "[p]ython" },
+        { "<leader>p_", hidden = true },
+        { "<leader>b", group = "[b]uffer" },
+        { "<leader>b_", hidden = true },
+        { "<leader>bc", group = "[c]lose" },
         { "<leader>bc_", hidden = true },
-        { "<leader>bs",  group = "[s]elect" },
+        { "<leader>bs", group = "[s]elect" },
         { "<leader>bs_", hidden = true },
-        { "<leader>c",   group = "[c]ode" },
-        { "<leader>c_",  hidden = true },
-        { "<leader>f",   group = "[f]ind" },
-        { "<leader>fG",  group = "[G]it" },
+        { "<leader>c", group = "[c]ode" },
+        { "<leader>c_", hidden = true },
+        { "<leader>f", group = "[f]ind" },
+        { "<leader>fG", group = "[G]it" },
         { "<leader>fG_", hidden = true },
-        { "<leader>f_",  hidden = true },
-        { "<leader>h",   group = "git [h]unk" },
-        { "<leader>h_",  hidden = true },
-        { "<leader>t",   group = "[t]oggle" },
-        { "<leader>t_",  hidden = true },
+        { "<leader>f_", hidden = true },
+        { "<leader>h", group = "git [h]unk" },
+        { "<leader>h_", hidden = true },
+        { "<leader>t", group = "[t]oggle" },
+        { "<leader>t_", hidden = true },
       })
 
       -- visual mode
       require("which-key").add({
-        { "<leader>h",  group = "git [h]unk" },
+        { "<leader>h", group = "git [h]unk" },
         { "<leader>h_", hidden = true },
-        { "<leader>f",  group = "[f]ind" },
+        { "<leader>f", group = "[f]ind" },
         { "<leader>f_", hidden = true },
       }, { mode = "v" })
     end,
